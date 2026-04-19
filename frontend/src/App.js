57b04c1fd6c0e68e75a5a10f31391bbfaf71041e
Deepsi Kumari <deepsikumari@gmail.com>
2026-04-19 12:15:31 +0530

    frontend buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,15 @@ import "./App.css";
 function App() {
   const [risks, setRisks] = useState([]);
   const [selected, setSelected] = useState(null);
-
+  const [filtered,setfiltered] = useState([]);
   useEffect(() => {
     fetch("http://localhost:8080/api/risk/all")
       .then((res) => res.json())
-      .then((data) => setRisks(data))
+      .then((data) => {
+       setRisks(data);
+       setfiltered(data);
+
+      })
       .catch((err) => console.error(err));
   }, []);
 
@@ -17,11 +21,34 @@ function App() {
     if (priority === "MEDIUM") return "orange";
     return "green";
   };
+ 
+  
+  const filterData = (type) => {
+    if (type === "ALL") {
+      setFiltered(risks);
+    } else {
+      setFiltered(risks.filter((r) => r.priority === type));
+    }
+  };
 
+  const count = (type) =>
+    risks.filter((r) => r.priority === type).length;
   return (
     <div className="container">
       <h1>🚨 Risk Dashboard</h1>
-
+      <div className="cards">
+        <div className="card">Total: {risks.length}</div>
+        <div className="card high">High: {count("HIGH")}</div>
+        <div className="card medium">Medium: {count("MEDIUM")}</div>
+        <div className="card low">Low: {count("LOW")}</div>
+      </div>
+      {/* FILTER BUTTONS */}
+      <div className="filters">
+        <button onClick={() => filterData("ALL")}>All</button>
+        <button onClick={() => filterData("HIGH")}>High</button>
+        <button onClick={() => filterData("MEDIUM")}>Medium</button>
+        <button onClick={() => filterData("LOW")}>Low</button>
+      </div>
       <table>
         <thead>
           <tr>
